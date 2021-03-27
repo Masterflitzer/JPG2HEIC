e05c94ba82b4b8a974b9a0bc1ea1d95eb496e142
Masterflitzer <60170948+Masterflitzer@users.noreply.github.com>
2021-03-27 16:39:28 +0100

    Update JPG2HEIC.sh

--- JPG2HEIC.sh
+++ JPG2HEIC.sh
@@ -2,21 +2,22 @@
 
 for x in *.jpg; do
     echo "$x"
-#    magick "$x" "${x%.jpg}.heic"
+    magick "$x" "${x%.jpg}.heic"
     exiftool -overwrite_original -tagsFromFile "$x" -all:all "${x%.jpg}.heic"
 
-    XMP="${x%.jpg}.XMP"
+    XMP="${x%.jpg}.xmp"
     if test -f "$XMP"; then
         exiftool -overwrite_original -tagsFromFile "$XMP" -all:all "${x%.jpg}.heic"
     fi  
 done
 
+
 for x in *.JPG; do
     echo "$x"
-#    magick "$x" "${x%.JPG}.heic"
+    magick "$x" "${x%.JPG}.heic"
     exiftool -overwrite_original -tagsFromFile "$x" -all:all "${x%.JPG}.heic"
 
-    XMP="${x%.JPG}.XMP"
+    XMP="${x%.JPG}.xmp"
     if test -f "$XMP"; then
         exiftool -overwrite_original -tagsFromFile "$XMP" -all:all "${x%.JPG}.heic"
     fi  
